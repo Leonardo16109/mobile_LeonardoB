ae4330657e6f8289f12789dc17d8b6c9a04e7788
Leonardo16109 <leonardo.golinski@escola.pr.gov.br>
2026-07-22 15:24:52 -0300

    Aula2 - ex01.js

---
+++ aula2/ex01.js
@@ -0,0 +1,6 @@
+function saudacao(nome) {
+    return Olá, ${nome}! Bons estudos.;
+}
+const prompt = require("prompt-sync")();
+const nome = prompt("Digite seu nome: ");
+console.log(saudacao(nome));
