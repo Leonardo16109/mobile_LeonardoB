a85cd8286bf1437f8f9f9fa200321443148a24c0
Leonardo16109 <leonardo.golinski@escola.pr.gov.br>
2026-07-22 15:36:46 -0300

    Lista1 - ex02.js

---
+++ lista1/ex02.js
@@ -0,0 +1,7 @@
+costbase = 10;
+const altura = 5;
+const area = base * altura;
+const perímetro = (base * 2) + (altura * 2);
+
+console.log(`Área do retangulo: ${area}`);
+console.log(`perímetro do retangulo: ${perímetro}`);
